572655a8dcfecbf036fab2a10e3627bfbd54d822
Cynthia <cmarechal@et.esiea.fr>
2022-04-14 12:53:20 +0200

    dernier commit

--- Multiplication-egyptienne.py
+++ Multiplication-egyptienne.py
@@ -1,37 +1,15 @@
-n = int(input("type a number : "))
-m = int(input("type another number : "))
-
-i = m
-z = 1
-list = []
-list2 = []
-j = 0
-a = n*m
-
-##multiplication entre n et m
-print(a)
-
-##liste des nombres inférieurs à n et m
-while n >= z :
-    list.append(z)
-    list2.append(i)
-    print("%(z)d| %(i)d" %{"z":z,"i":i})
-    z=2
-    i = m*z
-
-list = list[::-1]
-list2 = list2[::-1]
-
-list1=[]
+from re import A
 
 
-##opération permettant de trouver les nombres dont la somme est égale à nm
-for num in list2:
-    while num<=a:
-        a = a - num
-        list1.append(num)
+a = int(input("type a number : "))
+b = int(input("type another number : "))
+x = a
+y = b
+z = 0
 
-count = 0
-
-s = " + ".join([str(l) for l in list1])
-print("l'addition ",s, "=", n*m)
+while x != 0 :
+    if (x % 2) == 1 :
+        z = z + y 
+    y = y + y
+    x = int(x/2)
+    print(z)
